9471adad9d0c12565e13524223c022814582132e
MaryIv76 <ivanova_mary02@mail.ru>
2022-07-05 13:20:23 +0300

    Add .Net tasks' solutions

---
+++ .Net/2. Language basics/if-statements/IfStatements/Task14.cs
@@ -0,0 +1,55 @@
+﻿namespace IfStatements
+{
+    public static class Task14
+    {
+        public static int DoSomething(bool b1, bool b2, int i)
+        {
+            if (b1 && b2 && (i <= -5 || i > 5))
+            {
+                return 10 - (i * 2);
+            }
+
+            if (b1 && b2 && i > -5 && i <= 5)
+            {
+                return i * (-2);
+            }
+
+            if (b1 && !b2 && (i <= -5 || i > 5))
+            {
+                return i * i * i;
+            }
+
+            if (b1 && !b2 && i > -5 && i <= 5)
+            {
+                return i * i;
+            }
+
+            if ((!b1 && b2 && (i < -9 || i > 7)) || (!b1 && !b2 && (i < -9 || i > 7)))
+            {
+                return i * (-1);
+            }
+
+            if ((!b1 && b2 && ((i >= -9 && i < -7) || (i >= -3 && i <= 7))) || (!b1 && !b2 && ((i >= -9 && i < -3) || (i >= 5 && i <= 7))))
+            {
+                return i;
+            }
+
+            if (!b1 && b2 && i >= -7 && i < -3)
+            {
+                return i * 10;
+            }
+
+            if (!b1 && !b2 && ((i >= -3 && i < 0) || (i > 0 && i < 5)))
+            {
+                return i * (-100);
+            }
+
+            if (!b1 && !b2 && i == 0)
+            {
+                return 0;
+            }
+
+            return i;
+        }
+    }
+}
